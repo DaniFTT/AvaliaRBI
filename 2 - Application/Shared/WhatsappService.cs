8fa3c5f7aed6fd2f73b014a08731072ca2a8d806
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2024-03-03 21:15:21 -0300

    Remvido Twillow keys

--- 2 - Application/Shared/WhatsappService.cs
+++ 2 - Application/Shared/WhatsappService.cs
@@ -6,9 +6,9 @@ namespace AvaliaRBI._2___Application.Shared;
 
 public class WhatsappService
 {
-    private readonly string accountSid = "ACb3203a5d2869e29d059242e7124a974c";
-    private readonly string authToken = "f8495751d592b0c76a6c914634ede52a";
-    private readonly string fromWhatsAppNumber = "+14155238886";
+    private readonly string accountSid = "";
+    private readonly string authToken = "";
+    private readonly string fromWhatsAppNumber = "";
 
     public WhatsappService()
     {
@@ -17,11 +17,10 @@ public class WhatsappService
 
     public void SendMessage(string messageBody, string toWhatsAppNumber = null)
     {
-        var messageOptions = new CreateMessageOptions(new PhoneNumber($"whatsapp:{toWhatsAppNumber ?? "+5511973005415"}"))
+        var messageOptions = new CreateMessageOptions(new PhoneNumber($"whatsapp:{toWhatsAppNumber ?? ""}"))
         {
             From = new PhoneNumber($"whatsapp:{fromWhatsAppNumber}"),
             Body = messageBody,
-            MediaUrl = new List<Uri>() { new Uri("https://drive.google.com/uc?export=download&id=1VYfkcNq3xFQI91WwIhj6Ol1GqPMG3-oc") }
         };
 
         var message = MessageResource.Create(messageOptions);
